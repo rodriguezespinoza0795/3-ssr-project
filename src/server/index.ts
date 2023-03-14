716e4c6a4ed513c33b528694719c1d85442cc8ce
rodriguezespinoza0795 <rodriguezespinoza0795@gmail.com>
2023-03-14 09:50:21 -0600

    render HTML without react

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,14 @@
-console.log(("Hola Mundo desde el Server 2"))
+import express, { Express, Request, Response } from 'express';
+import { config } from './config';
+import { template } from './render/template';
+import { render } from './render';
+
+const app: Express = express();
+
+app.get('*', (req: Request, res: Response) => {
+    res.send(render(req.url))
+})
+
+app.listen(config.PORT, () => {
+    console.log(`linstening in http://localhost:${config.PORT}`)
+})
