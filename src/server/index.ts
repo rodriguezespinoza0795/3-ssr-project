2b962a2d8db5bac6ca300e669ac2cc1297cef682
rodriguezespinoza0795 <rodriguezespinoza0795@gmail.com>
2023-03-14 10:50:48 -0600

    work with hydrateRoot

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,10 @@
 import express, { Express, Request, Response } from 'express';
 import { config } from './config';
-import { template } from './render/template';
 import { render } from './render';
 
 const app: Express = express();
+
+app.use(express.static('dist'))
 
 app.get('*', (req: Request, res: Response) => {
     res.send(render(req.url))
